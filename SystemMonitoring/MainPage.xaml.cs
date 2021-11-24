38ed7b052e3819ba1281da178c8c411027b7c8ba
Çağlar Çavdar <caglarcavdar@trex.com.tr>
2021-11-24 10:16:19 +0300

    first commit

---
+++ SystemMonitoring/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace SystemMonitoring
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
